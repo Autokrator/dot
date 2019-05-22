ba8783bee0af498f84cbc22cd0b4918c0c8fac0b
Parth <parth.pthk18@gmail.com>
2019-05-22 09:47:54 -0400

    Tweak doom profile a bit

--- doom/config.el
+++ doom/config.el
@@ -1,6 +1,6 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(load-theme 'doom-one t)
+(load-theme 'doom-vibrant t)
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
@@ -19,13 +19,14 @@
   (doom-themes-treemacs-config)
   (set-face-attribute 'treemacs-root-face nil :height 130))
 (setq
+ doom-modeline-buffer-file-name-style 'truncate-with-project
  doom-modeline-icon t
+ doom-modeline-major-mode-icon t
  doom-modeline-major-mode-color-icon t)
 (map!
  :leader
  :prefix "w"
- :desc "delete window" "d" #'+workspace/close-window-or-workspace)
+ :desc "delete window" "d" #'ace-delete-window)
 (map!
  :leader
  :desc "Switch to last buffer" "TAB" #'evil-switch-to-windows-last-buffer)
-;; :desc "workspace" "W" doom-leader-workspace-map)
